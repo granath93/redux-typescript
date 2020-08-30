4e2c525cff2f0230faeb809c9a11df7994e065f2
Amanda Granath <amanda.granath@devies.se>
2020-08-30 13:58:15 +0200

    Creating a react app with redux, typescript and material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import theme from './theme';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './store/store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+      <Router>
+        <App />
+      </Router>
+  </ThemeProvider>
+  </Provider>,
+  document.querySelector('#root'),
 );
 
 // If you want your app to work offline and load faster, you can change
